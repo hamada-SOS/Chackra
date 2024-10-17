17b7d266302afd1ec955d06db50e1cb09aaec5d3
hamada-SOS <mohammadhassan241@gmail.com>
2024-10-17 07:41:07 +0300

    s

--- backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b21876dd96cdef3cb1608927a06ef65a308f7e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+074840dff07334df345f9c7025ddf534ca54e091")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
