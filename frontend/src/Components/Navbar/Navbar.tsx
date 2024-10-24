5244f91de71d503f8b12779befe302d6c0bbcad3
hamada-SOS <mohammadhassan241@gmail.com>
2024-10-24 20:51:14 +0300

    navbar

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,11 +1,28 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { text } from 'stream/consumers'
 
 interface Props {}
 
 const Navbar = (props: Props) => {
   return (
-    <Box sx={{width:'9.rem', height:'55px', background:'red'}}></Box>
+    <>
+        <Box sx={{width:'9.rem', height:'50px', background:'background.default', display:'flex', justifyContent:'space-between', alignItems:'center', pr:'20px', pl:'20px'}}>
+            <Box> 
+                <Typography>FOCSA eLab</Typography> {/* add the logo */}
+            </Box>
+            <Box sx={{display: 'flex', justifyContent:'space-around'}}>
+                <Link to={'/HomePage'} style={{ marginRight: '50px', textDecoration: 'none' }}><Typography sx={{color:'text.primary', '&:hover':{color:'primary.main'}}}>Home</Typography></Link>
+                <Link to={'/'} style={{ marginRight: '50px', textDecoration: 'none' }}><Typography sx={{color:'text.primary', '&:hover':{color:'primary.main'}}}>Problems</Typography></Link>
+                <Link to={'/'} style={{ marginRight: '50px',textDecoration: 'none' }}><Typography sx={{color:'text.primary', '&:hover':{color:'primary.main'}}}>Contest</Typography></Link>
+                <Link to={'/'} style={{textDecoration: 'none'}}><Typography sx={{color:'text.primary', '&:hover':{color:'primary.main'}}}>LeaderBoard</Typography></Link>
+            </Box>
+            <Box><Typography>Profile</Typography></Box>
+            
+        </Box>
+        <Box sx={{width:'9.rem', height:'0.5px', background:'black'}}></Box>
+    </>
   )
 }
 
